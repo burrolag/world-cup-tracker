3d105051a801c75de1da6e6a05ecb74aa0c276de
burrolag <166178149+burrolag@users.noreply.github.com>
2026-07-01 14:35:04 -0600

    updating vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/burrolag/world-cup-tracker',
+  base: '/world-cup-tracker/',
   test: {
     environment: "node",
     globals: true
